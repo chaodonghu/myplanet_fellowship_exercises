61662210822429e16ea3dff080fc8ed4f420d093
Dong Hu <cdhu@uwaterloo.ca>
2017-08-16 16:05:39 -0400

    Change app file name

---
+++ Day 3 - Node.js:Git/node-api-backend-skeleton/src/static/app.jsx
@@ -0,0 +1,14 @@
+ReactDOM.render(
+  <h1>Hello World!</h1>, document.getElementById('root')
+);
+
+//   fetch('http://localhost:8080/api/todos')
+//   .then((response) => response.json())
+//   .then((todos) => {
+//     todosNode.innerHTML = todos
+//     .map((todo) => {
+//       return `<li>${todo.text}</li>`;
+//     })
+//     .join('\n');
+//   });
+// };
